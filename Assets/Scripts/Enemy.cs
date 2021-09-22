bec5737a27e03f4728198a1427467c204ff1cada
alfi sahri <alfisahri33@gmail.com>
2021-09-22 21:22:25 +0700

    add enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public float Health = 50f;
 
-    // Update is called once per frame
-    void Update()
+  public UnityAction<GameObject> OnEnemyDestroyed = delegate { };
+
+  private bool _isHit = false;
+
+  void OnDestroy()
+  {
+    if (_isHit)
     {
-        
+      OnEnemyDestroyed(gameObject);
     }
+  }
+
+  void OnCollisionEnter2D(Collision2D col)
+  {
+    if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+    if (col.gameObject.tag == "Bird")
+    {
+      _isHit = true;
+      Destroy(gameObject);
+    }
+    else if (col.gameObject.tag == "Obstacle")
+    {
+      //Hitung damage yang diperoleh
+      float damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
+      Health -= damage;
+
+      if (Health <= 0)
+      {
+        _isHit = true;
+        Destroy(gameObject);
+      }
+    }
+  }
 }
