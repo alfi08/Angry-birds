7317fd43747bb5c81f7171eb127395378bd46739
alfi sahri <alfisahri33@gmail.com>
2021-09-22 21:00:08 +0700

    implemen bird and game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+  public SlingShooter SlingShooter;
+  public List<Bird> Birds;
+
+  void Start()
+  {
+    for (int i = 0; i < Birds.Count; i++)
+    {
+      Birds[i].OnBirdDestroyed += ChangeBird;
+    }
+
+    SlingShooter.InitiateBird(Birds[0]);
+  }
+
+  public void ChangeBird()
+  {
+    Birds.RemoveAt(0);
+
+    if (Birds.Count > 0)
+      SlingShooter.InitiateBird(Birds[0]);
+  }
+}
